5a282d09fc3907e17bdd2f87ede5c955ff1f4c99
astro-mat <whoismatwarren@gmail.com>
2024-06-02 18:45:45 +0000

    Create basic home page

--- StudioMooMooPortal/urls.py
+++ StudioMooMooPortal/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from booking.views import my_bookings
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('booking/', my_bookings, name='bookings'),
+    path("", include("booking.urls"))
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
-def my_bookings(request):
-    return HttpResponse("Hello, this is the booking page!")
+def home(request):
+    return render(request, "home.html")
