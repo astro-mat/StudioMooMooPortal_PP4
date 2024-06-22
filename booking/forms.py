2cdf8e1aedbad87e2a8cd6dc525a7ca9a98c1529
astro-mat <whoismatwarren@gmail.com>
2024-06-22 14:41:33 +0000

    Change database fields from datetime to date and time. Change related files

--- booking/forms.py
+++ booking/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('Artist_Name','start_time','end_time','BookingRequirements')
+        fields = ('Artist_Name','Date','start_time','end_time','Booking_Requirements')
         # Artist_Name = forms.CharField()
         # start_time = forms.DateTimeField()
         # end_time = forms.DateTimeField(widget=forms.DateInput(attrs={'type': 'date'}))
--- booking/models.py
+++ booking/models.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Artist_Name = models.CharField(max_length=30)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    BookingRequirements = models.CharField(max_length=700)
+    Date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    Booking_Requirements = models.CharField(max_length=700)
 
     def __str__(self):
         return self.Artist_Name
