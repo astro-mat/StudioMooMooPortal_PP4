84eb17a8e8f6ef7c59bd74f00c2fa01e6fa12f95
astro-mat <whoismatwarren@gmail.com>
2024-06-22 14:19:00 +0000

    Make more changes to forms to try to connect crispy forms

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,7 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('Artist_Name','start_time','end_time','BookingRequirements')
+        # Artist_Name = forms.CharField()
+        # start_time = forms.DateTimeField()
+        # end_time = forms.DateTimeField(widget=forms.DateInput(attrs={'type': 'date'}))
+        # BookingRequirements = forms.CharField()
