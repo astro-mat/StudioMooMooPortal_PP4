099b45f620aea6156f3a3a3082fd23cfc3a7ca7c
astro-mat <whoismatwarren@gmail.com>
2024-06-28 14:52:37 +0000

    Add date picker to forms

--- booking/forms.py
+++ booking/forms.py
@@ -1,11 +1,21 @@
 from .models import Booking
 from django import forms
 
+# class BookingForm(forms.ModelForm):
+#     class Meta:
+#         model = Booking
+#         fields = ('Artist_Name','Date','start_time','end_time','Booking_Requirements')
+
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('Artist_Name','Date','start_time','end_time','Booking_Requirements')
-        # Artist_Name = forms.CharField()
-        # start_time = forms.DateTimeField()
-        # end_time = forms.DateTimeField(widget=forms.DateInput(attrs={'type': 'date'}))
-        # BookingRequirements = forms.CharField()
+        fields = '__all__'
+        widgets = {
+            'Artist_Name': forms.Textarea(attrs={'cols': 60, 'rows': 1}),
+            'Date': forms.TextInput(attrs={'type': 'date'}),
+            'start_time': forms.TextInput(attrs={'type': 'time'}),
+            'end_time': forms.TextInput(attrs={'type': 'time'}),
+            'Booking_Requirements': forms.Textarea()
+        }
+        
