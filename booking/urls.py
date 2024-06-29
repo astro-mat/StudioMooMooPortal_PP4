585dca0227318b7a0017a39550b621598b5da822
astro-mat <whoismatwarren@gmail.com>
2024-06-29 19:52:38 +0000

    New page booking_success. Attempt to fix bug to clear form after making booking

--- booking/urls.py
+++ booking/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('my_bookings/', views.my_bookings, name='my_bookings'),
     path('edit_booking/<int:booking_id>', views.edit_booking, name='edit_booking'),
     path('delete_booking/<int:booking_id>', views.delete_booking, name='delete_booking'),
+    path('success_booking/<int:booking_id>', views.success_booking, name='success_booking'),
 ]
 
