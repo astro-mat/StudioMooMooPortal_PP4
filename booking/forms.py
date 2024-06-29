00a366314fbf22a41bd34f20004492be6696fc35
astro-mat <whoismatwarren@gmail.com>
2024-06-29 14:48:51 +0000

    Remove start time and end time from form.py

--- booking/forms.py
+++ booking/forms.py
@@ -1,21 +1,13 @@
 from .models import Booking
 from django import forms
 
-# class BookingForm(forms.ModelForm):
-#     class Meta:
-#         model = Booking
-#         fields = ('Artist_Name','Date','start_time','end_time','Booking_Requirements')
-
-
+        
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ('Artist_Name','Date','start_time','end_time','Booking_Requirements')
+        fields = ('Artist_Name','Date','Booking_Requirements')
         widgets = {
             'Artist_Name': forms.Textarea(attrs={'cols': 60, 'rows': 1}),
             'Date': forms.TextInput(attrs={'type': 'date'}),
-            'start_time': forms.TextInput(attrs={'type': 'time'}),
-            'end_time': forms.TextInput(attrs={'type': 'time'}),
             'Booking_Requirements': forms.Textarea()
         }
-        
