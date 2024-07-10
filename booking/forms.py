95bf892782141664f2ffa9fa4ec7590648e64028
Matthew Warren <whoismatwarren@gmail.com>
2024-07-10 22:07:14 +0000

    Attempt to add validation to date input

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,8 @@
-from .models import Booking
 from django import forms
+from django.forms.widgets import DateInput
+from django.core.exceptions import ValidationError
+from datetime import datetime, date
+from .models import Booking
 
         
 class BookingForm(forms.ModelForm):
@@ -7,7 +10,27 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ('artist_name','date','booking_requirements')
         widgets = {
-            'nrtist_name': forms.Textarea(attrs={'cols': 60, 'rows': 1}),
-            'date': forms.TextInput(attrs={'type': 'date'}),
+            'artist_name': forms.Textarea(attrs={'cols': 60, 'rows': 1}),
+            'date': DateInput(attrs={'type': 'date'}),
             'booking_requirements': forms.Textarea()
         }
+
+def clean(self):
+        """
+        Custom validation to ensure date and time are valid and available
+        """
+        cleaned_data = super().clean()
+        date = cleaned_data.get("date")
+
+        if date < date.today():
+            raise ValidationError("Please select a date in the future.")
+
+        existing_bookings = Booking.objects.filter(
+            date=date
+        ).exclude(id=self.instance.id)
+
+        if existing_bookings:
+            raise ValidationError(
+                "That date is already taken, "
+                "please select a different date."
+            )
