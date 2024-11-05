d62e4064414f9faa265f643b53bc66c5a3adc33b
Matthew Warren <whoismatwarren@gmail.com>
2024-11-05 14:38:46 +0000

    Chnages to requirements and env.py

--- StudioMooMooPortal/settings.py
+++ StudioMooMooPortal/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
     '8000-astromat-studiomoomoopo-98wi95ztyvt.ws-eu114.gitpod.io',
     '8000-astromat-studiomoomoopo-98wi95ztyvt.ws-eu115.gitpod.io',
     '8000-astromat-studiomoomoopo-x0cjatharr8.ws.codeinstitute-ide.net',
+    '8000-astromat-studiomoomoopo-6rk5i4ufhub.ws.codeinstitute-ide.net'
 ]
 
 
